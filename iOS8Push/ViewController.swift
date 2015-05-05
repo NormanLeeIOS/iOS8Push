6ba53d5a84a15e3179822310e3a802ea68781221
李亚坤 <285577079@qq.com>
2015-05-05 17:13:17 +0800

    no message

---
+++ iOS8Push/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS8Push
+//
+//  Created by 李亚坤 on 15/5/5.
+//  Copyright (c) 2015年 李亚坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
